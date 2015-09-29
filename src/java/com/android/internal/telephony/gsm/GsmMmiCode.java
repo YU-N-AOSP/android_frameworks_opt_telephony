39c0069cf8fc4b4049e249426389b32e7a4a42cb
Suchand Ghosh <suchan@codeaurora.org>
2015-09-29 11:05:23 +0530

    Telephony: Call forwarding icon is shown after powerup.
    If call forwarding was enabled, then the call forwarding
    icon should be shown after the phone has powered down and
    powered-up irrespective of SIM having EfCfis and EfCff.
    Added a preference parameter to store the call forwarding
    status, in case it's not updated correctly by the network
    operator.
    
    CRs-Fixed: 275722 795137 806156
    Change-Id: I616d2e73fd75a58fc1628bedb34126b00d31cba1

--- src/java/com/android/internal/telephony/gsm/GsmMmiCode.java
+++ src/java/com/android/internal/telephony/gsm/GsmMmiCode.java
@@ -1091,6 +1091,7 @@ public final class GsmMmiCode extends Handler implements MmiCode {
                     boolean cffEnabled = (msg.arg2 == 1);
                     if (mIccRecords != null) {
                         mPhone.setVoiceCallForwardingFlag(1, cffEnabled, mDialingNumber);
+                        mPhone.setCallForwardingPreference(cffEnabled);
                     }
                 }
 
@@ -1450,6 +1451,7 @@ public final class GsmMmiCode extends Handler implements MmiCode {
             boolean cffEnabled = (info.status == 1);
             if (mIccRecords != null) {
                 mPhone.setVoiceCallForwardingFlag(1, cffEnabled, info.number);
+                mPhone.setCallForwardingPreference(cffEnabled);
             }
         }
 
@@ -1484,6 +1486,7 @@ public final class GsmMmiCode extends Handler implements MmiCode {
                 // Set unconditional CFF in SIM to false
                 if (mIccRecords != null) {
                     mPhone.setVoiceCallForwardingFlag(1, false, null);
+                    mPhone.setCallForwardingPreference(false);
                 }
             } else {
 
