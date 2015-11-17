758320fac149533aae7cb7dd0ef3236d8efa2d4f
Muhammed Siju <msiju@codeaurora.org>
2015-11-17 20:57:06 +0530

    Fix LTE attach issue due to wrong initial attach profile.
    When SIM cards are changed, the attach profile set in modem is
    not changed until data is registered. Some times this leads to
    permanent LTE attach failure on LTE only networks.
    To fix this, de couple set initial attach apn from the setup data call
    apn list. Initial attach apn is always queried from 3GPP sim
    records as it is applicable only for LTE attach.
    DcTracker registers with 3GPP Iccrecords for records loaded event
    exclusively for setting initial attach apn.
    This makes set initial attach apn operation depend only on sim
    records loaded event irrespective of the data registration state.
    
    Change-Id: I4d7b1fe32f7c2ac1b5b0672e7c03bb489d8de596
    CRs-Fixed: 936560

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -1251,10 +1251,9 @@ public class DcTracker extends DcTrackerBase {
         return apn;
     }
 
-    protected ArrayList<ApnSetting> createApnList(Cursor cursor) {
+    protected ArrayList<ApnSetting> createApnList(Cursor cursor, IccRecords r) {
         ArrayList<ApnSetting> mnoApns = new ArrayList<ApnSetting>();
         ArrayList<ApnSetting> mvnoApns = new ArrayList<ApnSetting>();
-        IccRecords r = mIccRecords.get();
 
         if (cursor.moveToFirst()) {
             do {
@@ -2412,7 +2411,7 @@ public class DcTracker extends DcTrackerBase {
 
             if (cursor != null) {
                 if (cursor.getCount() > 0) {
-                    mAllApnSettings = createApnList(cursor);
+                    mAllApnSettings = createApnList(cursor, mIccRecords.get());
                 }
                 cursor.close();
             }
@@ -2432,7 +2431,7 @@ public class DcTracker extends DcTrackerBase {
             // TODO: What is the right behavior?
             //notifyNoData(DataConnection.FailCause.MISSING_UNKNOWN_APN);
         } else {
-            mPreferredApn = getPreferredApn();
+            mPreferredApn = getPreferredApn(mAllApnSettings);
             if (mPreferredApn != null && !mPreferredApn.numeric.equals(operator)) {
                 mPreferredApn = null;
                 setPreferredApn(-1);
@@ -2660,7 +2659,7 @@ public class DcTracker extends DcTrackerBase {
             usePreferred = true;
         }
         if (usePreferred) {
-            mPreferredApn = getPreferredApn();
+            mPreferredApn = getPreferredApn(mAllApnSettings);
         }
         if (DBG) {
             log("buildWaitingApns: usePreferred=" + usePreferred
@@ -2748,9 +2747,9 @@ public class DcTracker extends DcTrackerBase {
         }
     }
 
-    protected ApnSetting getPreferredApn() {
-        if (mAllApnSettings == null || mAllApnSettings.isEmpty()) {
-            log("getPreferredApn: mAllApnSettings is " + ((mAllApnSettings == null)?"null":"empty"));
+    protected ApnSetting getPreferredApn(ArrayList<ApnSetting> apnList) {
+        if (apnList == null || apnList.isEmpty()) {
+            log("getPreferredApn: apnList is " + ((apnList == null)?"null":"empty"));
             return null;
         }
 
@@ -2772,7 +2771,7 @@ public class DcTracker extends DcTrackerBase {
             int pos;
             cursor.moveToFirst();
             pos = cursor.getInt(cursor.getColumnIndexOrThrow(Telephony.Carriers._ID));
-            for(ApnSetting p : mAllApnSettings) {
+            for(ApnSetting p : apnList) {
                 log("getPreferredApn: apnSetting=" + p);
                 if (p.id == pos && p.canHandleType(mRequestedApnType)) {
                     log("getPreferredApn: X found apnSetting" + p);
@@ -2957,7 +2956,7 @@ public class DcTracker extends DcTrackerBase {
         return cid;
     }
 
-    private IccRecords getUiccRecords(int appFamily) {
+    protected IccRecords getUiccRecords(int appFamily) {
         return mUiccController.getIccRecords(mPhone.getPhoneId(), appFamily);
     }
 
